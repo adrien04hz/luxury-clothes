d5bd0de8b0dbe7151227d78241f0b2a91af38d2f
adrien04hz <adrien.ahs@gmail.com>
2026-03-13 20:13:47 -0600

    [UPDATE] Interfaz o servicio del catalogo de productos

--- services/producto/producto.service.ts
+++ services/producto/producto.service.ts
@@ -4,7 +4,36 @@
  * 6 de febrero de 2026
  */
 import { Producto as ProductoRepo } from '@/repositories/producto/producto.repository';
+
 export class Producto {
+
+    /**
+     * Función para obtener el catálogo de productos
+     * @param id_genero - ID del género para filtrar productos (opcional)
+     * @param id_categoria - ID de la categoría para filtrar productos (opcional)
+     * @param id_subcategoria - ID de la subcategoría para filtrar productos (opcional)
+     * @param id_marca - ID de la marca para filtrar productos (opcional)
+     * @returns Lista de productos que coinciden con los filtros aplicados
+     */
+    static async getCatalog({
+        id_genero,
+        id_categoria,
+        id_subcategoria,
+        id_marca
+    }: {
+        id_genero?: number;
+        id_categoria?: number;
+        id_subcategoria?: number;
+        id_marca?: number;
+    }) {
+        return await ProductoRepo.getAllProducts({
+            id_genero,
+            id_categoria,
+            id_subcategoria,
+            id_marca
+        });
+    }
+
     static async getProductDetails(productId: number) {
         return await ProductoRepo.productDetails(productId);
     }
