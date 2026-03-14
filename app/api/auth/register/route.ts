695d196b3680127f1b0834ffde7aa5918f7448a0
AbdielCervantes4 <abdielcervantes822@gmail.com>
2026-03-13 20:33:34 -0600

    [UPDATE] Refactorizacion de parametros

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -8,8 +8,25 @@ import { NextResponse } from "next/server";
 import { AuthService } from "@/services/cliente/cliente.service";
 
 export async function POST(req: Request) {
+
   try {
+
     const body = await req.json();
+
+    const {
+      nombre,
+      apellidos,
+      correo,
+      contrasena,
+      telefono
+    } = body;
+
+    if (!nombre || !apellidos || !correo || !contrasena || !telefono) {
+      return NextResponse.json(
+        { error: "Todos los campos son obligatorios" },
+        { status: 400 }
+      );
+    }
 
     await AuthService.register(body);
 
@@ -23,5 +40,7 @@ export async function POST(req: Request) {
       { error: error.message },
       { status: 400 }
     );
+
   }
+
 }
