66cd9e6860162ab83949307659650d8db1c4f58f
valm <valm040723@gs.utm.mx>
2026-04-21 23:52:40 -0600

    [UPDATE] Actualizar estado del envio

--- app/logistica/estadoenvio/page.tsx
+++ app/logistica/estadoenvio/page.tsx
@@ -10,6 +10,11 @@ export default function EnviosPendientes(){
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
 
+  //Manejar actualizacion de estado de envio
+  const [openModal, setOpenModal] = useState(false);
+  const [pedidoSeleccionado, setPedidoSeleccionado] = useState<number | null>(null);
+  const [nuevoEstado, setNuevoEstado] = useState("");
+
   const getEstadoColor = (estado: EnvioPendiente["estado_envio"]): string => {
     switch (estado) {
       case "Pendiente": return "bg-green-500 text-white";
@@ -57,6 +62,47 @@ export default function EnviosPendientes(){
     loadEnviosPendientes();
   }, []);
 
+  const estadosMap: Record<string, number> = {
+  Pendiente: 1,
+  Preparado: 2,
+  Enviado: 3,
+  "En Camino": 4,
+  Entregado: 5
+};
+  const actualizarEstado = async () => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const idUsuarioLogistica = 1; // o sacarlo del JWT si quieres hacerlo PRO
+
+    const res = await fetch("/api/logistica/estado_envio", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        idPedido: pedidoSeleccionado,
+        idNuevoEstado: estadosMap[nuevoEstado],
+        idUsuarioLogistica
+      })
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(data.error);
+
+    setOpenModal(false);
+    setNuevoEstado("");
+
+    // 🔥 recargar lista
+    loadEnviosPendientes();
+
+  } catch (err: any) {
+    setError(err.message);
+  }
+};
+
   return (
     <div className=" pl-8 bg-gray-50 font-sans pb-10">
       <main className="p-6 ">
@@ -88,7 +134,7 @@ export default function EnviosPendientes(){
                 </div>
               </div>
 
-              <div className="space-y-2 mb-6 border-l-2 border-gray-100 pl-4">
+              <div className="space-y-2 mb-6 border-l-2 border-gray-200 pl-2">
                 
                 <div className="flex items-center gap-2 text-gray-600">
                   <Clock size={14} className="text-black" />
@@ -100,20 +146,67 @@ export default function EnviosPendientes(){
                 </div>
               </div>
 
-              <button 
-                disabled={envio.estado_envio === "Entregado"}
+              <button
+                onClick={() => {
+                  setPedidoSeleccionado(envio.id_pedido);
+                  setOpenModal(true);
+                }}
                 className={`flex items-center justify-center gap-2 font-black p-4 text-xs uppercase tracking-widest transition shadow-lg rounded-full ${
                   envio.estado_envio === "Entregado" 
                   ? "bg-gray-100 text-gray-400 cursor-not-allowed" 
                   : "bg-black text-white hover:bg-gray-800 active:scale-95"
                 }`}
               >
-                Confirmar Entrega
+                Modificar estado
               </button>
             </div>
           ))}
         </section>
+
+          {openModal && (
+  <div className="fixed inset-0 bg-black/40 flex items-center justify-center">
+    <div className="bg-white p-6 rounded-xl w-80 space-y-4">
+
+      <h3 className="text-lg font-bold">Cambiar estado</h3>
+
+      <select
+        className="w-full border p-2 rounded"
+        value={nuevoEstado}
+        onChange={(e) => setNuevoEstado(e.target.value)}
+      >
+        <option value="">Selecciona estado</option>
+        <option value="Preparado">Preparado</option>
+        <option value="Enviado">Enviado</option>
+        <option value="En Camino">En Camino</option>
+        <option value="Entregado">Entregado</option>
+      </select>
+
+      <div className="flex justify-end gap-2">
+        <button
+          onClick={() => setOpenModal(false)}
+          className="px-4 py-2 text-sm"
+        >
+          Cancelar
+        </button>
+
+        <button
+          onClick={async () => {
+            await actualizarEstado();
+          }}
+          className="bg-black text-white px-4 py-2 text-sm rounded"
+        >
+          Confirmar
+        </button>
+      </div>
+    </div>
+  </div>
+)}
+  
+ 
       </main>
     </div>
+
+    
   );
+ 
 };
