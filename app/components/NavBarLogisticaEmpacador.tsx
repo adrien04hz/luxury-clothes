39b58d6e36b816a0bae8c23823e98b6ac513b3b7
Luisdiazantonio <luisdiaz09446@gmail.com>
2026-04-29 16:54:05 -0600

    [FIX] correccion de nombre de function en el rol 4

--- app/components/NavBarLogisticaEmpacador.tsx
+++ app/components/NavBarLogisticaEmpacador.tsx
@@ -1,38 +1,47 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
-export default function NavBarLogistica() {
-  const router = useRouter();
+export default function NavBarLogisticaEmpacador() {
+  const [rol, setRol] = useState<number>(0);
+
+  useEffect(() => {
+    const rol = localStorage.getItem("rol");
+    setRol(rol ? parseInt(rol, 10) : 0);
+
+  }, []);
 
   return (
-    <div className="w-full h-28 bg-black text-white flex justify-between px-12">
+    <>
+    <div className="w-full h-28 bg-black text-white flex items-center justify-between px-12">
       <Link href="/logistica/estadopedido" className="flex items-center justify-center">
-        <Image
-          src="/assets/logo/main-logo.svg"
-          alt="Logo"
-          width={174}
-          height={80}
+        <Image 
+          src="/assets/logo/main-logo.svg" 
+          alt="Logo" 
+          loading="eager"
+          width={174} 
+          height={80} 
         />
       </Link>
 
+       <nav className="*:h-full flex items-center">
+        <ul className="h-full flex justify-center items-center text-md space-x-9">
+          <li>
+            <p className="text-3xl">{rol === 3 ? "Repartidor" : "Empacador"}</p>
+          </li>
+          </ul>        
+      </nav>      
       <div className="flex space-x-7 items-center">
         <div className="flex space-x-5">
-          <button
-            onClick={() => {
-              localStorage.removeItem("token");
-              localStorage.removeItem("rol");
-              localStorage.removeItem("userID");
-              router.push("/");
-            }}
-            className="px-5 py-2 text-sm border border-white rounded-lg hover:bg-gray-900 transition"
-          >
-            Cerrar Sesión
-          </button>
+         
+          <Link href="/cuenta">
+            <Image src="/assets/images/profile.svg" alt="User" width={30} height={30} />
+          </Link>
+         
         </div>
       </div>
     </div>
+    </>
   );
 }
